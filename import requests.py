cf9f6ffd2c0a3c0ecf26e897a0f696b68cf64003
Jordan Lee <jordanlee2017@gmail.com>
2024-09-17 19:28:59 -0400

    repass of balance sheet data pull

--- import requests.py
+++ import requests.py
@@ -7,7 +7,7 @@ from openpyxl import Workbook
 # Configuration
 CLIENT_ID = 'AB11725981363YVFLMqg7tO2OpRZyk1x2aQ60WUPAL1aA7OdKY'
 CLIENT_SECRET = 'jA88goCfvChG2HBf5oHTrOhSLu1CTMd6FqL2IoAG'
-ACCESS_TOKEN = 'eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..GUa4KlR8L9x4w1C-gWK6pg.ykzy1m1U8qKgpTj6e4u9lOFA2cAVK2KFidKyDeqApx1E-SyeTZOGPU3cW1NTU1ZgHpQEBK7xOYS2HY8LjVW7U7b-7l6PsAaWrsSvSu_nNe7zHmSuP4OV6B8mmDL1HZlWC0R9HQDjenCvAo51TERQ0vTvcCkPOyPrrZKVQKGC9bVNHlBvDZeYyCM7Y7yt3bn_3WOxr4t_7F1vL6HmjOMAtdiZIEzzzAYfSP4ePEMqwiuAeXPYMEj5yWJpQ8pTHczyg5zJ3JMZHOdT97GIPcb9c93Wq7Kr9qCMaiJmh5hLeGEHAuAORDXL1yv9bi4wE6BTLQx0hvc5FNHndEfBFHq8lOYDbYAvNZDXAFsywoy1TDPk_uwRwxPD_zq0RC-kDmqzE0ft8r6PInGrv8Sb79RtnXTskeMKLd6NtchSNaDQyX9A-njhlOop4wFdj3GOp0WRu2_DqcOPNbRC8xuYrEq_yjfJbQp3LM-3pWao-lJ_rNWipP_f6uGvR6rUndKlIUG3Q8wAN8XwkouCCofQZxWmhrkLUrraGhio-L5JbrZ__O73772kY3gChGey2f1KaYjc4_k_RWSKaGu6Tm-Z1PGvbCj4VEYg67QnpDqzDsYnGuNApexJJ-8wzOw2XWWj3r3PqqVNDf1c6zxhP5shc1GoQ449HnAPZ4CosExivJSoFfP4vhysp7SnvPcO7t6jbNSiXiiXD_2mDbwpivrNQhL-C9Tzhk8IcGduJptePZt8vZWxQ75zMmFR04LfedSWfXDk.vqSqN8ktfQsYK0oYVDYqgg'
+ACCESS_TOKEN = 'eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..C_suFv2uL2IoLBIpH735GQ.Q6a61mhj5h2C8ZbyGbQQGvhm_EeZ0ShKfvdywf60IdNd3rfb6-m6j_Wk628Bm0wrNz0vVo1kH8R18j2UUHwFpkscTttuktms9X6evzAZOCzobJWcKLy9S3HSNhLzQeVp2xAQrxjY4ZUImTO7IyFUJxQGUjyZFR3YYVD4_EPO-Iea-avYWPW9MoHnLceGcM0K_sPW7wuxHfD71tWe0jpU6jXZgBKwgIryQyW4-kZ8gccLhrovstwf5YAQOyEzmTlVIUem1lGGcJZ0ML6Wo_yUGr0urlwQ41355CrGVnmYPtB5bhpIyxuRsx17iBdZ6-GbKj7mGoiPLOJxweGboejilohxWeNKO2MlEoBvjh-GQ_MR76XB28XXGaUP87DjCkYM-qXyaZZGJOUSPmGuQiG9ZC4J7aWZweg_8vZd_RhCEwFBNbELb0CQ6s77NhJ2BOnZT_vjOvJ3EbLjv46eTJrnLrk9NrUkVTLmBcd-816__FpJQDY4tv5PrQ5rNzza0dARk9YA_W2lIMPm24hpVC1cLY0ADpC6I-7bC_lzxC-wFP7ic6eZoxCkJj75Q2yX6DIvrrIupZCZmW4TQ7ASbB0T4h7K0WIDYhhkp0L7x_a98PcG4baGzJyY1VyKbBMMUS_ZumoDt7gve_4tUEZwXt1pWhz2dkd9q8LjftEYenIxof8yZ3hsvH9vWlviH0gf8aqXJ1hBJOYADl7YfufUyMX0OA4Vf7ToMtsoLVtkvx8Un-MwtzeN9SxnSDBnJyhirPTq.vuzPrIG5OitbknCjXBAVYA'
 COMPANY_ID = '9341452910276277'
 
 # QuickBooks API endpoints
@@ -37,32 +37,57 @@ if response.status_code == 200:
     
     # Prepare a list to hold the data
     data = []
-    '''def parse_balance_sheet(data):
-        balance_sheet = data.get('Rows', {}).get('Row', [])
-        headers = []
-        values = []
+        
 
-        for row in balance_sheet:
-            cells = row.get('Cells', [])
-            if not headers:
-                headers = [cell.get('value') for cell in cells]
-            else:
-                values.append([cell.get('value') for cell in cells])
-
-        return headers, values'''
-    
-
+    # Loop through rows of data
     for row in rows:
+        # Check if 'Rows' exists within the current row
+        if 'Rows' in row:
 
-        '''if 'Summary' in row:
-            data.append({
-                'Label': row['Summary']['ColData'][0]['value'],
-                'Amount': row['Summary']['ColData'][1]['value']
-            })'''
+            temp = list(row['Rows']['Row'][0].items())
+            res = [idx for idx, key in enumerate(temp)]
+            #print(res)
+            #print(row['Rows']['Row'])
+            #print(row['Rows']['Row'][0]['Rows']['Row'][0]['Rows']['Row'][0])
+            # Parse the key data necessary for financial forecasting
+            i = 0
+            for key in row['Rows']['Row'][0]['Rows']['Row']:
+                
+                try:
+                    account_name = key['Rows']['Row'][1]['ColData'][0].get('value', 'N/A')
+                    value = key['Rows']['Row'][1]['ColData'][1].get('value', '0.00')
+                    print(account_name)
+                    print(value)
+                    data.append({
+                        'Account Name': account_name,
+                        'Value': value
+                    })
+                except IndexError:
+                    print("IndexError")
+                    continue
+                except KeyError:
+                    print("KeyError")
+                    continue
 
-        # Append the extracted data to the list
-        data.append(row)
+                i+=1
+                print(data)
+                #print(key['Summary']['ColData'][0]['value'])
+                #print(i)
+                #print(row['Rows']['Row'][key])
 
+            '''# Loop through sub-rows in 'Rows'
+            for sub_row in row['Rows']['Row']:
+                if 'ColData' in sub_row:
+                    account_name = sub_row['Header']['ColData'][0]['value'] if sub_row['Header']['ColData'][0]['value'] else 'N/A'
+                    value = sub_row['Header']['ColData'][1]['value'] if sub_row['Header']['ColData'][1]['value'] else '0.00'
+                    print(account_name)
+                    
+                    # Add the parsed data into the list
+                    data.append({
+                        'Account Name': account_name,
+                        'Value': value
+                    })'''
+            
     # Convert the list to a DataFrame
     #df = pd.DataFrame(values, columns=headers)
     df = pd.DataFrame(data)
