3fa71ca54085df87ac0c15d97f2bb6447ec34bbb
Jordan Lee <jordanlee2017@gmail.com>
2024-12-03 16:25:16 -0500

    test

--- active_client_portal.py
+++ active_client_portal.py
@@ -274,7 +274,7 @@ if st.session_state.get('authenticated'):
             
             # Create the filtered earnings DataFrame
             for col in review_cols:
-                col = col.strftime("%B %Y")
+                col = datetime.strptime(str(col), "%B %Y")
             earnings_df = pd.concat(
                 [income_row, gm_row, noi_row],
                 axis=0
